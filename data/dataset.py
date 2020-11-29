6890fcfb2fa3278539827da879f69545d6d326b6
jiahaunluo <luojiahuan001@gmail.com>
2020-11-29 16:20:01 +0800

    initial commit

--- data/dataset.py
+++ data/dataset.py
@@ -76,3 +76,59 @@ class ImageDataset(Dataset):
             return (image, path, labels)
         else:
             raise Exception('Unknown mode : {}'.format(self._mode))
+
+
+
+class MyImageDataset(Dataset):
+    def __init__(self, label_path, cfg, mode='train'):
+        self.cfg = cfg
+        self._label_header = None
+        self._image_paths = []
+        self._labels = []
+        self._mode = mode
+        # self.dict = [{'1.0': '1', '': '0', '0.0': '0', '-1.0': '0'},
+        #              {'1.0': '1', '': '0', '0.0': '0', '-1.0': '1'}, ]
+        with open(label_path) as f:
+            header = f.readline().strip('\n').split(',')
+            self._label_header = [
+                header[1],
+                header[2],
+                header[3],
+                header[4],
+                header[5]]
+            for line in f:
+                fields = line.strip('\n').split(',')
+                image_path = fields[0]
+                labels = fields[1:]
+                labels = list(map(int, labels))
+                self._image_paths.append(image_path)
+                assert os.path.exists(image_path), image_path
+                self._labels.append(labels)
+                if flg_enhance and self._mode == 'train':
+                    for i in range(self.cfg.enhance_times):
+                        self._image_paths.append(image_path)
+                        self._labels.append(labels)
+        self._num_image = len(self._image_paths)
+
+    def __len__(self):
+        return self._num_image
+
+    def __getitem__(self, idx):
+        image = cv2.imread(self._image_paths[idx], 0)
+        image = Image.fromarray(image)
+        if self._mode == 'train':
+            image = GetTransforms(image, type=self.cfg.use_transforms_type)
+        image = np.array(image)
+        image = transform(image, self.cfg)
+        labels = np.array(self._labels[idx]).astype(np.float32)
+
+        path = self._image_paths[idx]
+
+        if self._mode == 'train' or self._mode == 'dev':
+            return (image, labels)
+        elif self._mode == 'test':
+            return (image, path)
+        elif self._mode == 'heatmap':
+            return (image, path, labels)
+        else:
+            raise Exception('Unknown mode : {}'.format(self._mode))
